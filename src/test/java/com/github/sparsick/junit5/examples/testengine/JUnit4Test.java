5f4a5250c71305f786769307ed3e0d74615d9170
sparsick <sparsick@web.de>
2018-12-06 09:00:48 +0100

    add junit4 example

---
+++ src/test/java/com/github/sparsick/junit5/examples/testengine/JUnit4Test.java
@@ -0,0 +1,13 @@
+package com.github.sparsick.junit5.examples.testengine;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JUnit4Test {
+
+    @Test
+    public void testDemo(){
+        assertTrue(true);
+    }
+}
