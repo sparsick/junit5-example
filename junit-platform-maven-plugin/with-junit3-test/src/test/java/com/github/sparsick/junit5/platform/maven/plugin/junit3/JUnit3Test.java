c5923f9cd18249ed04ae4bb818cdb85920d59976
sparsick <sparsick@web.de>
2019-01-15 08:56:07 +0100

    refactoring

---
+++ junit-platform-maven-plugin/with-junit3-test/src/test/java/com/github/sparsick/junit5/platform/maven/plugin/junit3/JUnit3Test.java
@@ -0,0 +1,11 @@
+package com.github.sparsick.junit5.platform.maven.plugin.junit3;
+
+import junit.framework.TestCase;
+
+public class JUnit3Test extends TestCase{
+
+    public void testMethod(){
+        assertTrue(true);
+    }
+
+}
