a0796c76b2d6b8a6492a81f64e6effc643b560e2
sparsick <sparsick@web.de>
2019-01-20 09:23:17 +0100

    fix

--- junit5-in-pre-java8-projects/src/main/java/Java7Class.java
+++ junit5-in-pre-java8-projects/src/main/java/Java7Class.java
@@ -5,6 +5,6 @@ public class Java7Class {
     public void aMethod(){
         System.out.println("Hello World");
 
-        Function<String, String > java8Feature = (input) -> input;
+//        Function<String, String > java8Feature = (input) -> input;
     }
 }
