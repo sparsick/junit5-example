13de328d1c0a77654f30fafbad9d0868f9bc6658
sparsick <sparsick@web.de>
2019-01-20 08:30:04 +0100

    add using junit5 in pre java 8 projects

---
+++ junit5-in-pre-java8-projects/src/main/java/Java7Class.java
@@ -0,0 +1,10 @@
+import java.util.function.Function;
+
+public class Java7Class {
+
+    public void aMethod(){
+        System.out.println("Hello World");
+
+        Function<String, String > java8Feature = (input) -> input;
+    }
+}
---
+++ junit5-in-pre-java8-projects/src/test/java/JUnit5Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+class JUnit5Test {
+
+
+    @Test
+    void testJava7Feature(){
+        Java7Class java7 = new Java7Class();
+
+        java7.aMethod();
+    }
+}
